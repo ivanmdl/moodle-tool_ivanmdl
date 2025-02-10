2ec5c61b88a5aaf3e68e587723e1751632ba9f75
kidza <dzaivan@gmail.com>
2025-02-10 21:18:53 +0100

    initial commit

---
+++ version.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   tool_ivanmdl
+ * @copyright 2025, Ivan Stankovic <ivan.stankovic@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2025021000;
+//$plugin->requires = 2022041900.00; // Moodle 4.0.
+//$plugin->supported = [400, 400];
+//$plugin->incompatible = 401;
+$plugin->component = 'tool_ivanmdl';
+$plugin->release = '1.0';
